f442744ce3ab3603d4f410884f4142756b5fc203
Cody <vipcody@gmail.com>
2016-11-21 16:22:43 -0500

    Create DisableAddToCartButton.js

---
+++ DisableAddToCartButton.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+function DisableAddToCardButton()
+{
+    document.getElementById("Anthem_ctl00_MainContentHolder_AddToCartButton1_btnAdd__").onclick = function ()
+    {
+        this.disabled = true;
+    }
+    document.getElementById("ctl00_MainContentHolder_AddToCartButton1_btnAdd").onclick = function ()
+    {
+        this.disabled = true;
+    }
+}
+</script>
